bf7adb06c34d29828a1ce34b482f85c358924493
WangHeYao <1874373481@qq.com>
2024-09-20 15:59:46 +0800

    fix
    fix

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("node:path");
+// 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
+console.log("Hello from Electron 👋");
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+
+    // win.loadFile("index.html");
+    win.loadURL("https://api.minsuwuliu.com/login?redirect=%2Fexpress%2FreciOrder");
+
+    // win.webContents.on("did-finish-load", () => {
+    //     win.webContents.print({ silent: false, printBackground: true }, (success, failureReason) => {
+    //         if (!success) console.log(failureReason);
+    //         console.log("Print initiated");
+    //     });
+    // });
+};
+
+app.whenReady().then(() => {
+    ipcMain.handle("ping", () => "pong");
+
+    createWindow();
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});
